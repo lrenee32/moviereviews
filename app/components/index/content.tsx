8026ebf8e5f8fb873bfc91e129bee51c90d5f383
Jacob Adcock <51841765+jadcock32@users.noreply.github.com>
2022-08-05 17:28:13 -0500

    added headlines, side bar to index

--- app/components/index/content.tsx
+++ app/components/index/content.tsx
@@ -9,6 +9,7 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Chip from '@mui/material/Chip';
+import { SectionDivider } from 'components/shared/section-divider';
 import formatDistanceToNowStrict from 'date-fns/formatDistanceToNowStrict';
 import { VARIABLES } from 'assets/themes/themes';
 
@@ -47,70 +48,118 @@ export const Content: FunctionComponent<Props> = (props: Props) => {
   };
 
   return (
-    <Box sx={{ background: `linear-gradient(to right, ${VARIABLES.primaryColor} 0%, ${VARIABLES.bgColor} 20%, ${VARIABLES.bgColor} 80%, ${VARIABLES.primaryColor} 100%)` }}>
-      <Container fixed sx={{ paddingY: '100px', backgroundColor: VARIABLES.bgColor }}>
-        <Box display="flex" alignItems="center" justifyContent="space-between" mb="70px">
-          <Box>
-            <Typography variant="h3" marginBottom="15px">Highlights Today</Typography>
-            <Typography variant="h4" sx={{ opacity: '.5' }}>Be sure not to miss these reviews.</Typography>
+    <Box sx={{ background: `linear-gradient(to bottom, #0D090A 50%, ${VARIABLES.primaryColor} 150%)` }}>
+      <Container fixed sx={{ paddingY: '100px', background: VARIABLES.bgColor, boxShadow: '0px 0px 25px 0px rgba(0,0,0,0.50);' }}>
+        <Box mb="70px">
+          <Box display="flex" alignItems="center" justifyContent="space-between" mb="70px">
+            <Box>
+              <Typography variant="h3" marginBottom="15px">Highlights Today</Typography>
+              <Typography variant="h4" sx={{ opacity: '.5' }}>Be sure not to miss these reviews.</Typography>
+            </Box>
+            <Box display="flex" alignItems="center">
+              <Chip
+                label="Recent"
+                sx={{ ml: '5px' }}
+                onClick={() => filterAction('recent')}
+                variant={filterType === 'recent' ? 'filled': 'outlined'}
+                size="large"
+              />
+              <Chip
+                label="Site Picks"
+                sx={{ ml: '5px' }}
+                onClick={() => filterAction('pick')}
+                variant={filterType === 'pick' ? 'filled': 'outlined'}
+                size="large"
+              />
+              <Chip
+                label="Top Rated"
+                sx={{ ml: '5px' }}
+                onClick={() => filterAction('top')}
+                variant={filterType === 'top' ? 'filled': 'outlined'}
+                size="large"
+              />
+            </Box>
           </Box>
-          <Box display="flex" alignItems="center">
-            <Chip
-              label="Recent"
-              sx={{ ml: '5px' }}
-              onClick={() => filterAction('recent')}
-              variant={filterType === 'recent' ? 'filled': 'outlined'}
-              size="large"
-            />
-            <Chip
-              label="Site Picks"
-              sx={{ ml: '5px' }}
-              onClick={() => filterAction('pick')}
-              variant={filterType === 'pick' ? 'filled': 'outlined'}
-              size="large"
-            />
-            <Chip
-              label="Top Rated"
-              sx={{ ml: '5px' }}
-              onClick={() => filterAction('top')}
-              variant={filterType === 'top' ? 'filled': 'outlined'}
-              size="large"
-            />
-          </Box>
+          <Grid container spacing={2}>
+            {filtered.map((entry: Entry<Review>) => {
+              return (
+                <Grid key={entry.EntryId} item xs={6} md={3}>
+                  <Card sx={{ backgroundImage: 'none', backgroundColor: 'transparent', boxShadow: 'none' }}>
+                    <CardActionArea href={`/entry/${entry.EntryId}`}>
+                      <CardMedia
+                        component="img"
+                        height="100%"
+                        image={getPosterImage(entry.Details!.FilmPoster)}
+                        alt={`${entry.Title}-${entry.EntryId}`}
+                      />
+                    </CardActionArea>
+                    <CardContent sx={{ paddingX: '0' }}>
+                      <Typography lineHeight="1" mb="2.5px" variant="h6">{entry.Details!.FilmTitle}</Typography>
+                      <Typography fontSize="12px" color="primary" sx={{ textTransform: 'uppercase', mb: '5px' }}>| { formatDistanceToNowStrict(entry.Created) } ago</Typography>
+                      <Box>
+                        <Chip
+                          label={ `IMDB: ${entry.Details!.TMDBRating}` }
+                          color="primary"
+                          sx={{ marginRight: '5px' }}
+                        />
+                        <Chip
+                          label={ `Personal: ${entry.Details!.UserRating}` }
+                          color="primary"
+                        />
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              );
+            })}
+          </Grid>
         </Box>
-        <Grid container spacing={2}>
-          {filtered.map((entry: Entry<Review>) => {
-            return (
-              <Grid key={entry.EntryId} item xs={6} md={3}>
-                <Card sx={{ backgroundImage: 'none', backgroundColor: 'transparent', boxShadow: 'none' }}>
-                  <CardActionArea href={`/entry/${entry.EntryId}`}>
+        <SectionDivider text="Latest Headlines" />
+        <Box display="flex">
+          <Box width="70%">
+            {All.map(entry => (
+              <Card key={`${entry.Title}-${entry.EntryId}-headline`} sx={{ mt: '20px', mr: '20px'}}>
+                <CardActionArea href={`/entry/${entry.EntryId}`} sx={{ display: 'flex', alignItems: 'flex-start' }}>
+                  <CardMedia
+                    component="img"
+                    image={entry.Details!.FeaturedImage}
+                    alt={`${entry.Title}-${entry.EntryId}-headline`}
+                    sx={{ width: '50%' }}
+                  />
+                  <CardContent sx={{ width: '50%', mt: "30px" }}>
+                    <Typography fontSize="12px" color="primary" sx={{ textTransform: 'uppercase', mb: '5px' }}>{entry.Type} | { formatDistanceToNowStrict(entry.Created) } ago</Typography>
+                    <Typography variant="h4">
+                      {entry.Title}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+          </Box>
+          <Box width="30%" position="sticky" alignSelf="flex-start" top="0">
+            <SectionDivider text="Latest Reviews" />
+            {clonedArr.filter(i => i.Type === 'review').splice(0, 4).map(entry => {
+              return (
+                <Card key={`${entry.Title}-${entry.EntryId}-headline`} sx={{ mt: '20px', mr: '20px'}}>
+                  <CardActionArea href={`/entry/${entry.EntryId}`} sx={{ display: 'flex', justifyContent: 'flex-start' }}>
                     <CardMedia
                       component="img"
-                      height="100%"
                       image={getPosterImage(entry.Details!.FilmPoster)}
-                      alt={`${entry.Title}-${entry.EntryId}`}
+                      alt={`${entry.Title}-${entry.EntryId}-latest-reviews`}
+                      sx={{ height: '80px', width: '80px'}}
                     />
+                    <CardContent sx={{ display: 'flex', flexDirection: 'column' }}>
+                      <Typography fontSize="12px" color="primary" sx={{ textTransform: 'uppercase', mb: '5px' }}>{entry.Type} | { formatDistanceToNowStrict(entry.Created) } ago</Typography>
+                      <Typography>
+                        {entry.Details!.FilmTitle}
+                      </Typography>
+                    </CardContent>
                   </CardActionArea>
-                  <CardContent sx={{ paddingX: '0' }}>
-                    <Typography lineHeight="1" mb="2.5px" variant="h6">{entry.Title}</Typography>
-                    <Typography fontSize="12px" color="primary" sx={{ textTransform: 'uppercase', mb: '5px' }}>| { formatDistanceToNowStrict(entry.Created) } ago</Typography>
-                    <Box>
-                      <Chip
-                        label={ `IMDB: ${entry.Details!.TMDBRating}` }
-                        color="primary"
-                        sx={{ marginRight: '5px' }}
-                      />
-                      <Chip
-                        label={ `Personal: ${entry.Details!.UserRating}` }
-                        color="primary"
-                      />
-                    </Box>
-                  </CardContent>
                 </Card>
-              </Grid>
-            );
-          })}
-        </Grid>
+              );
+            })}
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
---
+++ app/components/shared/section-divider.tsx
@@ -0,0 +1,38 @@
+import { FunctionComponent } from 'react';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import Typography from '@mui/material/Typography';
+import { VARIABLES } from 'assets/themes/themes';
+
+interface Props {
+  text: string,
+};
+
+export const SectionDivider: FunctionComponent<Props> = (props: Props) => {
+  const { text } = props;
+
+  return (
+    <Box position="relative" display="flex" justifyContent="center" mb="20px">
+      <Divider
+        variant="middle"
+        sx={{
+          mb: '20px',
+          position: 'absolute',
+          width: '100%',
+          top: '50%',
+          transform: 'translate(0, -50%)'
+        }}
+      />
+      <Typography
+        variant="h5"
+        sx={{
+          backgroundColor: VARIABLES.primaryColor,
+          padding: '2.5px 10px',
+          transform: 'skewX(-15deg)'
+        }}
+      >
+        {text}
+      </Typography>
+    </Box>
+  )
+};
