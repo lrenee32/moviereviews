111cd87ad935fd758494b7a1ebd36603b16a2e24
jadcock32 <jacob.r.adcock@gmail.com>
2022-08-30 00:17:57 -0500

    finish google analytics setup

--- app/components/shared/consent.tsx
+++ app/components/shared/consent.tsx
@@ -1,34 +1,67 @@
+// @ts-nocheck
+
 import { FunctionComponent, useEffect, useState } from 'react';
 import { setCookie, hasCookie } from 'cookies-next';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import { VARIABLES } from 'assets/themes/themes';
 
-const Consent: FunctionComponent = () => {
-  const [consent, setConsent] = useState<boolean>(true);
+const CookieConsent: FunctionComponent = () => {
+  const [consent, setConsent] = useState<boolean>(false);
 
   useEffect(() => {
     setConsent(hasCookie('acceptCookies'));
   }, []);
 
-  const acceptCookies = () => {
+  const setCookieState = (state: boolean) => {
     setConsent(true);
-    setCookie('acceptCookies', 'true', { maxAge: 60 * 60 * 24 * 365 });
-  };
-
-  const close = () => {
-    setConsent(true);
-  };
-
-  const denyCookie = () => {
-    setConsent(true);
-    setCookie('acceptCookies', 'false', { maxAge: 60 * 60 * 24 * 365 });
+    setCookie('acceptCookies', `${state}`, { maxAge: 60 * 60 * 24 * 365 });
+    if (state) {
+      gtag('consent', 'update', {
+        ad_storage: 'granted',
+        analytics_storage: 'granted',
+      });
+    }
   };
 
   return (
     <>
-      {consent && (
-        <div>Hey</div>
+      {!consent && (
+        <AppBar position="fixed" sx={{ bottom: '0', top: 'auto', backgroundColor: VARIABLES.bgColor }}>
+          <Box display="flex" flexDirection="column" alignItems="center" m="20px 20px 10px 20px">
+            <Typography mb="10px" textAlign="center">
+              By clicking "Allow All", you agree to the storing of cookies on your device to enhance site navigation and to analyze site usage.
+            </Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexWrap: 'wrap',
+                '> button': {
+                  borderColor: VARIABLES.primaryColor,
+                  color: VARIABLES.primaryColor,
+                  marginBottom: '10px',
+                  '&:not(:last-of-type)': {
+                    marginRight: '10px',
+                  },
+                  '&:hover': {
+                    borderColor: VARIABLES.primaryColor,
+                    backgroundColor: VARIABLES.primaryColor,
+                    color: '#FFF'
+                  }
+                }
+              }}
+            >
+              <Button variant="outlined" onClick={() => setCookieState(true)}>Allow All</Button>
+              <Button variant="outlined" onClick={() => setCookieState(false)}>Deny All</Button>
+            </Box>
+          </Box>
+        </AppBar>
       )}
     </>
   );
 };
 
-export default Consent;
+export default CookieConsent;
--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,34 +1,62 @@
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
+// @ts-nocheck
+
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import '../assets/styles/normalize.css';
 import { global } from '../assets/themes/themes';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { ScrollToTop } from 'components/shared/scoll-to-top';
-import { pageview } from '../lib/ga';
+import { ScrollToTop } from '../components/shared/scoll-to-top';
+import CookieConsent from '../components/shared/consent';
+import { getCookie } from 'cookies-next';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  const consent = getCookie('acceptCookies');
   const theme = createTheme(global(createTheme()));
-
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      pageview(url);
-    };
-
-    router.events.on('routeChangeComplete', handleRouteChange);
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-    }
-  }, [router.events]);
   
   return (
     <ThemeProvider theme={theme}>
+      <Script
+        id="gtag"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied'
+            });
+
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GOOGLE_TAGS_MANAGER}');
+          `,
+        }}
+      />
+      {consent && (
+        <Script
+          id="consupd"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            gtag('consent', 'update', {
+              'ad_storage': 'granted',
+              'analytics_storage': 'granted'
+            });
+          `,
+          }}
+        />
+      )}
       <CssBaseline />
+      <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GOOGLE_TAGS_MANAGER}`}
+        height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
       <Component {...pageProps} />
       <ScrollToTop />
+      <CookieConsent />
     </ThemeProvider>
   );
 };
